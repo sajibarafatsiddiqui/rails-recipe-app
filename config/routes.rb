ae9d8bfc123c7b86f38199e829d69212318f0e47
Solomon <solobarine@gmail.com>
2023-06-01 13:38:53 +0100

    Add shopping list route

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :recipes, only: [:index, :show]
   resources :inventories
   resources :public_recipes, only: [:index, :show]
-  resources :shopping_lists, only: [:show]
+  resources :shopping_lists, only: [:index, :show]
+  get "/shopping_lists?recipe_id=3&inventory_id=6", to: 'shopping_list#show'
 end
