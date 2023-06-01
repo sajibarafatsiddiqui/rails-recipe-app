fe75709ea3112aea6fe1ed8afaa258e4d5a3dfae
Solomon <solobarine@gmail.com>
2023-06-01 13:33:34 +0100

    Create controllers for Shopping List and Public Recipes

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,14 @@
+class ShoppingListsController < ApplicationController
+    def index
+        @list = 'Shopping List'
+    #     recipe = Recipe.find(params[:recipe_id])
+    #     inventory = Inventory.find(params[:inventory_id])
+    end
+
+    def create
+        
+    end
+
+    def show
+    end
+end
